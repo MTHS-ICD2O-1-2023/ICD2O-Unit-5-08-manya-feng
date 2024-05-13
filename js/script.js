fbfde252a8e35938d780d8991bc263c2978eb4bc
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-13 20:46:31 +0000

    change

--- js/script.js
+++ js/script.js
@@ -8,21 +8,24 @@
 
 let counter = 0
 let answer = 0
-let answera = firstinputNumber
+let remainder = secondinputNumber
 
 function myButtonClicked() {
   const firstinputNumber = parseInt(document.getElementById("firstinputNumber").value)
   const secondinputNumber = parseInt(document.getElementById("secondinputNumber").value)
   
-  while (answera >= secondinputNumber) {
-    answera -= secondinputNumber 
-    counter++
+  while (true) {
+    if (remainder < firstinputNumber){
+      break;
+    }
+    remainder -= firstinputNumber;
+    counter++;
   }
 
   answer = counter
   
   document.getElementById("answer").innerHTML =
-    firstinputNumber + " / " + secondinputNumber + " = " + answer + "R:" + answera
+    firstinputNumber + " / " + secondinputNumber + " = " + answer + "R" + answera
   }
 
 
